519e17415805deb81cd7911f3d0c7108d6af1142
Robin <robin.kriisa@voco.ee>
2022-12-13 11:36:47 +0200

    JS CSS added

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3002
 
+// add template engine
+const path = require('path')
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//static files - CSS, JS, Images
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
 })
 
 app.listen(port, () => {
